f8422b7b7dc873d92803c96ab5b6a34ce2bc9ddd
wiso <giurrero@gmail.com>
2014-06-06 08:31:00 +0200

    new layout

---
+++ js/jquery.main.js
@@ -0,0 +1,489 @@
+// page init
+jQuery(function(){
+	initFixedNav();
+	initScroll();
+});
+
+// fixed nav init
+function initFixedNav() {
+	jQuery('body').fixedSlideBlock({
+		maxWidth:800, //px
+		slideBlock: '#nav',
+		positionType: 'fixed'
+	});
+};
+
+// scroll page init
+function initScroll(){
+	var animSpeed = 1000; //ms
+	var maxWidth = 800; //px
+	var activeClass = 'active';
+	var win = jQuery(window);
+	var nav = jQuery('#nav');
+	var links = nav.find('a');
+	links.each(function(){
+		var link = jQuery(this);
+		var href = link.attr('href');
+		link.bind('click',function(e){
+			jQuery('html,body').animate({scrollTop:jQuery(href).offset().top - (win.width() > maxWidth ? nav.outerHeight() : 0)},{duration: animSpeed, queue: false});
+			e.preventDefault();
+		});
+	});
+};
+
+// Fixed Slide Block plugin
+;(function(){
+	jQuery.fn.fixedSlideBlock = function(options) {
+		var options = jQuery.extend({
+			slideBlock: '#sidebar',
+			alwaysStick: true,
+			animDelay: 100,
+			animSpeed: 250,
+			extraHeight: 0,
+			maxWidth: 9999,
+			positionType: 'auto' // 'fixed', 'absolute'
+		}, options);
+		
+		return this.each(function(){
+			var win = jQuery(window);
+			var holder = jQuery(this);
+			var fixedBlock = holder.find(options.slideBlock);
+			var positionType = options.positionType, timer, d, stayStatic, prevHeight;
+			var maxWidth = options.maxWidth;
+			
+			// skip if block does not exists
+			if(!fixedBlock.length) {
+				return;
+			}
+			
+			// detect positioning type
+			if(positionType === 'auto') {
+				positionType = !isFixedPositionSupported || isTouchDevice ? 'absolute' : 'fixed';
+			}
+			
+			// recalculate all values
+			function recalculateDimensions() {
+				var origStyle = fixedBlock.attr('style');
+				fixedBlock.css({position:'',left:'',top:'',bottom:''});
+				d = {
+					winHeight: win.height(),
+					scrollTop: win.scrollTop(),
+					scrollLeft: win.scrollLeft(),
+					holderOffset: holder.offset(),
+					holderHeight: holder.height(),
+					blockPosition: fixedBlock.position(),
+					blockOffset: fixedBlock.offset(),
+					blockHeight: fixedBlock.outerHeight(true)
+				};
+				fixedBlock.attr('style',origStyle);
+				
+				// check for static position
+				if(prevHeight !== d.holderHeight) {
+					prevHeight = d.holderHeight;
+					stayStatic = checkStaticPosition();
+				}
+			}
+			
+			// dont fix block if content too small
+			function checkStaticPosition() {
+				var origStyle = fixedBlock.attr('style');
+				var origHeight = d.holderHeight;
+				fixedBlock.css({position:'absolute'});
+				var newHeight = holder.height();
+				if(newHeight < origHeight) {
+					fixedBlock.css({position:'', top:'', left:'', bottom:''});
+					return true;
+				} else {
+					fixedBlock.attr('style',origStyle);
+				}
+			}
+			
+			function positionFixed() {
+				if(d.scrollTop > d.blockOffset.top - options.extraHeight && maxWidth < win.width()) {
+					// check that block fits in holder
+					if(d.scrollTop + options.extraHeight - d.holderOffset.top + d.blockHeight > d.holderHeight) {
+						fixedBlock.css({position:'absolute', left: d.blockPosition.left, top: d.blockPosition.top + d.holderHeight - d.blockHeight - (d.blockOffset.top - d.holderOffset.top), bottom:'auto'});
+					}
+					// set default fixed position
+					else {
+						fixedBlock.css({position:'fixed', left: d.blockOffset.left - d.scrollLeft, top: options.extraHeight, bottom:'auto'});
+					}
+				} else {
+					// disable sticking
+					fixedBlock.css({position:'', top:'', left:'', bottom:''});
+				}
+			}
+			
+			function positionAbsolute(noAnimation) {
+				// default top position
+				var top = d.blockPosition.top;
+				if(d.scrollTop > d.blockOffset.top - options.extraHeight) {
+					// check that block fits in holder
+					if(d.scrollTop + options.extraHeight - d.holderOffset.top + d.blockHeight > d.holderHeight) {
+						top = d.blockPosition.top + d.holderHeight - d.blockHeight - (d.blockOffset.top - d.holderOffset.top);
+					}
+					// set fixed position
+					else {
+						top = d.blockPosition.top + d.scrollTop - d.blockOffset.top + options.extraHeight;
+					}
+				}
+				fixedBlock.stop().css({position:'absolute', left: d.blockPosition.left, bottom:'auto'});
+				
+				// change block position animation
+				if(noAnimation === true) {
+					fixedBlock.css({top: top});
+				} else {
+					fixedBlock.animate({top: top},{duration: options.animSpeed});
+				}
+			}
+			
+			// reposition function
+			function reposition(e) {
+				// detect behavior
+				var noAnimation = (e === true);
+				
+				// recalculate size and offsets
+				recalculateDimensions();
+				if(stayStatic) {
+					return;
+				}
+				
+				// disable when window is smaller then fixed block
+				if(!options.alwaysStick && d.winHeight < d.blockHeight) {
+					fixedBlock.css({position:'', top:'', left:'',bottom:''});
+					return;
+				}
+				
+				// call position handler
+				if(positionType === 'fixed') {
+					positionFixed();
+				} else {
+					if(noAnimation) {
+						positionAbsolute(noAnimation);
+					}
+					clearTimeout(timer);
+					timer = setTimeout(positionAbsolute, options.animDelay);
+				}
+			}
+			
+			// add event handlers
+			setTimeout(function() {
+				reposition(true);
+				win.bind('scroll', reposition);
+				win.bind('resize orientationchange', function(){
+					reposition(true);
+				});
+			},10);
+		});
+	};
+	
+	// detect device type
+	var isTouchDevice = (function() {
+		try {
+			return ('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch;
+		} catch (e) {
+			return false;
+		}
+	}());
+	
+	// detect position fixed support
+	var isFixedPositionSupported = (function(){
+		var supported = false, container = document.createElement('div'), fixedBlock = document.createElement('div');
+		container.style.cssText = 'position:absolute;top:9px;left:9px;width:100px;height:100px;background:red;';
+		fixedBlock.style.cssText = 'position:fixed;top:7px;width:1px;height:1px;';
+		container.appendChild(fixedBlock);
+		document.documentElement.insertBefore(container, document.documentElement.childNodes[0]);
+		supported = (fixedBlock.offsetTop == 7 || fixedBlock.offsetTop == -2 || (!fixedBlock.offsetWidth && typeof document.documentElement.style.maxHeight !== 'undefined'));
+		document.documentElement.removeChild(container);
+		fixedBlock = container = null;
+		return supported;
+	}());
+}());
+
+// html5shiv MIT @rem remysharp.com/html5-enabling-script
+// iepp v1.6.2 MIT @jon_neal iecss.com/print-protector
+/*@cc_on(function(m,c){var z="abbr|article|aside|audio|canvas|details|figcaption|figure|footer|header|hgroup|mark|meter|nav|output|progress|section|summary|time|video";function n(d){for(var a=-1;++a<o;)d.createElement(i[a])}function p(d,a){for(var e=-1,b=d.length,j,q=[];++e<b;){j=d[e];if((a=j.media||a)!="screen")q.push(p(j.imports,a),j.cssText)}return q.join("")}var g=c.createElement("div");g.innerHTML="<z>i</z>";if(g.childNodes.length!==1){var i=z.split("|"),o=i.length,s=RegExp("(^|\\s)("+z+")",
+"gi"),t=RegExp("<(/*)("+z+")","gi"),u=RegExp("(^|[^\\n]*?\\s)("+z+")([^\\n]*)({[\\n\\w\\W]*?})","gi"),r=c.createDocumentFragment(),k=c.documentElement;g=k.firstChild;var h=c.createElement("body"),l=c.createElement("style"),f;n(c);n(r);g.insertBefore(l,
+g.firstChild);l.media="print";m.attachEvent("onbeforeprint",function(){var d=-1,a=p(c.styleSheets,"all"),e=[],b;for(f=f||c.body;(b=u.exec(a))!=null;)e.push((b[1]+b[2]+b[3]).replace(s,"$1.iepp_$2")+b[4]);for(l.styleSheet.cssText=e.join("\n");++d<o;){a=c.getElementsByTagName(i[d]);e=a.length;for(b=-1;++b<e;)if(a[b].className.indexOf("iepp_")<0)a[b].className+=" iepp_"+i[d]}r.appendChild(f);k.appendChild(h);h.className=f.className;h.innerHTML=f.innerHTML.replace(t,"<$1font")});m.attachEvent("onafterprint",
+function(){h.innerHTML="";k.removeChild(h);k.appendChild(f);l.styleSheet.cssText=""})}})(this,document);@*/
+
+/*! Respond.js v1.0.1pre: min/max-width media query polyfill. (c) Scott Jehl. MIT/GPLv2 Lic. j.mp/respondjs  */
+(function( win, mqSupported ){
+	//exposed namespace
+	win.respond		= {};
+	
+	//define update even in native-mq-supporting browsers, to avoid errors
+	respond.update	= function(){};
+	
+	//expose media query support flag for external use
+	respond.mediaQueriesSupported	= mqSupported;
+	
+	//if media queries are supported, exit here
+	if( mqSupported ){ return; }
+	
+	//define vars
+	var doc 			= win.document,
+		docElem 		= doc.documentElement,
+		mediastyles		= [],
+		rules			= [],
+		appendedEls 	= [],
+		parsedSheets 	= {},
+		resizeThrottle	= 30,
+		head 			= doc.getElementsByTagName( "head" )[0] || docElem,
+		links			= head.getElementsByTagName( "link" ),
+		requestQueue	= [],
+		
+		//loop stylesheets, send text content to translate
+		ripCSS			= function(){
+			var sheets 	= links,
+				sl 		= sheets.length,
+				i		= 0,
+				//vars for loop:
+				sheet, href, media, isCSS;
+
+			for( ; i < sl; i++ ){
+				sheet	= sheets[ i ],
+				href	= sheet.href,
+				media	= sheet.media,
+				isCSS	= sheet.rel && sheet.rel.toLowerCase() === "stylesheet";
+
+				//only links plz and prevent re-parsing
+				if( !!href && isCSS && !parsedSheets[ href ] ){
+					// selectivizr exposes css through the rawCssText expando
+					if (sheet.styleSheet && sheet.styleSheet.rawCssText) {
+						translate( sheet.styleSheet.rawCssText, href, media );
+						parsedSheets[ href ] = true;
+					} else {
+						if( !/^([a-zA-Z]+?:(\/\/)?)/.test( href )
+							|| href.replace( RegExp.$1, "" ).split( "/" )[0] === win.location.host ){
+							requestQueue.push( {
+								href: href,
+								media: media
+							} );
+						}
+					}
+				}
+			}
+			makeRequests();
+				
+		},
+		
+		//recurse through request queue, get css text
+		makeRequests	= function(){
+			if( requestQueue.length ){
+				var thisRequest = requestQueue.shift();
+				
+				ajax( thisRequest.href, function( styles ){
+					translate( styles, thisRequest.href, thisRequest.media );
+					parsedSheets[ thisRequest.href ] = true;
+					makeRequests();
+				} );
+			}
+		},
+		
+		//find media blocks in css text, convert to style blocks
+		translate			= function( styles, href, media ){
+			var qs			= styles.match(  /@media[^\{]+\{([^\{\}]+\{[^\}\{]+\})+/gi ),
+				ql			= qs && qs.length || 0,
+				//try to get CSS path
+				href		= href.substring( 0, href.lastIndexOf( "/" )),
+				repUrls		= function( css ){
+					return css.replace( /(url\()['"]?([^\/\)'"][^:\)'"]+)['"]?(\))/g, "$1" + href + "$2$3" );
+				},
+				useMedia	= !ql && media,
+				//vars used in loop
+				i			= 0,
+				j, fullq, thisq, eachq, eql;
+
+			//if path exists, tack on trailing slash
+			if( href.length ){ href += "/"; }	
+				
+			//if no internal queries exist, but media attr does, use that	
+			//note: this currently lacks support for situations where a media attr is specified on a link AND
+				//its associated stylesheet has internal CSS media queries.
+				//In those cases, the media attribute will currently be ignored.
+			if( useMedia ){
+				ql = 1;
+			}
+			
+
+			for( ; i < ql; i++ ){
+				j	= 0;
+				
+				//media attr
+				if( useMedia ){
+					fullq = media;
+					rules.push( repUrls( styles ) );
+				}
+				//parse for styles
+				else{
+					fullq	= qs[ i ].match( /@media ([^\{]+)\{([\S\s]+?)$/ ) && RegExp.$1;
+					rules.push( RegExp.$2 && repUrls( RegExp.$2 ) );
+				}
+				
+				eachq	= fullq.split( "," );
+				eql		= eachq.length;
+				
+					
+				for( ; j < eql; j++ ){
+					thisq	= eachq[ j ];
+					mediastyles.push( { 
+						media	: thisq.match( /(only\s+)?([a-zA-Z]+)(\sand)?/ ) && RegExp.$2,
+						rules	: rules.length - 1,
+						minw	: thisq.match( /\(min\-width:[\s]*([\s]*[0-9]+)px[\s]*\)/ ) && parseFloat( RegExp.$1 ), 
+						maxw	: thisq.match( /\(max\-width:[\s]*([\s]*[0-9]+)px[\s]*\)/ ) && parseFloat( RegExp.$1 )
+					} );
+				}	
+			}
+
+			applyMedia();
+		},
+        	
+		lastCall,
+		
+		resizeDefer,
+		
+		//enable/disable styles
+		applyMedia			= function( fromResize ){
+			var name		= "clientWidth",
+				docElemProp	= docElem[ name ],
+				currWidth 	= doc.compatMode === "CSS1Compat" && docElemProp || doc.body[ name ] || docElemProp,
+				styleBlocks	= {},
+				dFrag		= doc.createDocumentFragment(),
+				lastLink	= links[ links.length-1 ],
+				now 		= (new Date()).getTime();
+			
+			//throttle resize calls	
+			if( fromResize && lastCall && now - lastCall < resizeThrottle ){
+				clearTimeout( resizeDefer );
+				resizeDefer = setTimeout( applyMedia, resizeThrottle );
+				return;
+			}
+			else {
+				lastCall	= now;
+			}
+										
+			for( var i in mediastyles ){
+				var thisstyle = mediastyles[ i ];
+				if( !thisstyle.minw && !thisstyle.maxw || 
+					( !thisstyle.minw || thisstyle.minw && currWidth >= thisstyle.minw ) && 
+					(!thisstyle.maxw || thisstyle.maxw && currWidth <= thisstyle.maxw ) ){						
+						if( !styleBlocks[ thisstyle.media ] ){
+							styleBlocks[ thisstyle.media ] = [];
+						}
+						styleBlocks[ thisstyle.media ].push( rules[ thisstyle.rules ] );
+				}
+			}
+			
+			//remove any existing respond style element(s)
+			for( var i in appendedEls ){
+				if( appendedEls[ i ] && appendedEls[ i ].parentNode === head ){
+					head.removeChild( appendedEls[ i ] );
+				}
+			}
+			
+			//inject active styles, grouped by media type
+			for( var i in styleBlocks ){
+				var ss		= doc.createElement( "style" ),
+					css		= styleBlocks[ i ].join( "\n" );
+				
+				ss.type = "text/css";	
+				ss.media	= i;
+				
+				if ( ss.styleSheet ){ 
+		        	ss.styleSheet.cssText = css;
+		        } 
+		        else {
+					ss.appendChild( doc.createTextNode( css ) );
+		        }
+		        dFrag.appendChild( ss );
+				appendedEls.push( ss );
+			}
+			
+			//append to DOM at once
+			head.insertBefore( dFrag, lastLink.nextSibling );
+		},
+		//tweaked Ajax functions from Quirksmode
+		ajax = function( url, callback ) {
+			var req = xmlHttp();
+			if (!req){
+				return;
+			}	
+			req.open( "GET", url, true );
+			req.onreadystatechange = function () {
+				if ( req.readyState != 4 || req.status != 200 && req.status != 304 ){
+					return;
+				}
+				callback( req.responseText );
+			}
+			if ( req.readyState == 4 ){
+				return;
+			}
+			req.send( null );
+		},
+		//define ajax obj 
+		xmlHttp = (function() {
+			var xmlhttpmethod = false;	
+			try {
+				xmlhttpmethod = new XMLHttpRequest();
+			}
+			catch( e ){
+				xmlhttpmethod = new ActiveXObject( "Microsoft.XMLHTTP" );
+			}
+			return function(){
+				return xmlhttpmethod;
+			};
+		})();
+	
+	//translate CSS
+	ripCSS();
+	
+	//expose update for re-running respond later on
+	respond.update = ripCSS;
+	
+	//adjust on resize
+	function callMedia(){
+		applyMedia( true );
+	}
+	if( win.addEventListener ){
+		win.addEventListener( "resize", callMedia, false );
+	}
+	else if( win.attachEvent ){
+		win.attachEvent( "onresize", callMedia );
+	}
+})(
+	this,
+	(function( win ){
+		
+		//for speed, flag browsers with window.matchMedia support and IE 9 as supported
+		if( win.matchMedia ){ return true; }
+
+		var bool,
+			doc			= document,
+			docElem		= doc.documentElement,
+			refNode		= docElem.firstElementChild || docElem.firstChild,
+			// fakeBody required for <FF4 when executed in <head>
+			fakeUsed	= !doc.body,
+			fakeBody	= doc.body || doc.createElement( "body" ),
+			div			= doc.createElement( "div" ),
+			q			= "only all";
+			
+		div.id = "mq-test-1";
+		div.style.cssText = "position:absolute;top:-99em";
+		fakeBody.appendChild( div );
+		
+		div.innerHTML = '_<style media="'+q+'"> #mq-test-1 { width: 9px; }</style>';
+		if( fakeUsed ){
+			docElem.insertBefore( fakeBody, refNode );
+		}	
+		div.removeChild( div.firstChild );
+		bool = div.offsetWidth == 9;  
+		if( fakeUsed ){
+			docElem.removeChild( fakeBody );
+		}	
+		else{
+			fakeBody.removeChild( div );
+		}
+		return bool;
+	})( this )
+);
